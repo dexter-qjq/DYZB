56f6f6257c4ba616691527c25b2a8bcea966d1e6
dexter-qjq <qjq793437528@163.com>
2021-12-22 13:50:18 +0800

    初始化项目

---
+++ DouYu/DouYu/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DouYu
+//
+//  Created by 璟 on 2021/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DouYu/DouYu/DouYuApp.swift
@@ -0,0 +1,17 @@
+//
+//  DouYuApp.swift
+//  DouYu
+//
+//  Created by 璟 on 2021/12/22.
+//
+
+import SwiftUI
+
+@main
+struct DouYuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
